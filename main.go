55687fe74231a01fea6bd64f1df744521f15f6f2
bashery <ahmedalbachiry@gmail.com>
2021-12-31 02:02:46 +0300

    some helper functions

--- main.go
+++ main.go
@@ -5,47 +5,37 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+	"strings"
 	"sync"
 
 	"github.com/melbahja/goph"
 )
 
-var wg sync.WaitGroup
-
-type Host struct {
-	Address    string `json:"address"`
-	Password   string `json:"password"`
-	ClientName string `json:"clientName"`
-	Active     bool   `json:"active"`
-}
-
 func main() {
-	hosts, err := loadHosts("hosts.json")
-	if err != nil {
-		log.Fatalln("err whith loadHosts() function:\n", err)
-	}
 
 	allhosts, err := loadHosts("hosts.json")
+	activeHosts := filterActive(allhosts)
 	checkErr("loadHosts:", err)
 
-	activeHosts := filterActive(allhosts)
-	fmt.Println("all hosts is : ")
-	for _, host := range allhosts {
-		fmt.Println(host.ClientName)
+	client, err := goph.NewUnknown("root", activeHosts[0].Address, goph.Password(psw))
+	checkErr("goph.NewUnknown():", err)
+	defer client.Close()
+	output, err := client.Run("ls")
+	if err != nil {
+		fmt.Println(err)
 	}
+	fmt.Println(string(output))
 
-	fmt.Println()
-	fmt.Println("active hosts is : ")
-	for _, host := range activeHosts {
-		fmt.Println(host.ClientName)
-	}
-	os.Rename("test", activeHosts[0].ClientName)
+	//zip(client, activeHosts[0].ClientName, activeHosts[0].ClientName)
 
 	os.Exit(0)
 
+	os.Rename("test", activeHosts[0].ClientName)
+
 	// importent
-	for _, host := range hosts {
+	for _, host := range activeHosts {
 		host := host
 
 		fmt.Println(host.Address)
@@ -53,12 +43,12 @@ func main() {
 		go func() {
 			defer wg.Done()
 
-			client, err := goph.NewUnknown("root", host.Address, goph.Password(host.Password))
+			client, err := goph.NewUnknown("root", host.Address, goph.Password(psw))
 			checkErr("goph.NewUnknown():", err)
 			defer client.Close()
 			log.Println("ssh client oppend, Done")
 			// Upload new bot app to new host
-			err = client.Upload("hosts.json", "/root/hosts.json")
+			err = client.Upload("disactive.json", "/root/hosts.json")
 			checkErr("error with deploy(): ", err)
 
 			// run lineBot in new host
@@ -69,13 +59,75 @@ func main() {
 
 		}()
 	}
-	wg.Wait()
-
-	// mybe we need enabling bot as a service with systemkd
 
 	// check new client in clientFile one per huor
 
 	// deploying bot to this client
+	wg.Wait()
+}
+
+var wg sync.WaitGroup
+
+type Helper struct{}
+
+var h Helper
+
+// writeData update/rewrite data into file
+func (Helper) writeData(file, data string) error {
+	err := os.WriteFile(file, []byte(data+"\n"), 0644)
+	if err != nil {
+		log.Println(err)
+	}
+	return err
+	//defer f.Close()
+	//if _, err := f.WriteString(data + "\n"); err != nil {
+	//	log.Println(err)
+	//}
+}
+
+// loadDisactive load addresses of disactive hosts
+func (Helper) loadDisactive(path string) ([]string, error) {
+
+	bin, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	return strings.Split(string(bin), "\n"), nil
+}
+
+// appendData append new address to addressfile
+func (Helper) appendData(file, data string) {
+	f, err := os.OpenFile(file,
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
+	if _, err := f.WriteString(data + "\n"); err != nil {
+		log.Println(err)
+	}
+}
+
+// filterList make list unique
+func (Helper) filterList(data []string) []string {
+	mp := make(map[string]bool)
+	for _, h := range data {
+		mp[h] = true
+	}
+	hosts := make([]string, 0)
+	for h := range mp {
+		if h == "" {
+			break
+		}
+		hosts = append(hosts, h)
+	}
+	return hosts
+}
+
+type Host struct {
+	Address    string `json:"address"`
+	ClientName string `json:"clientName"`
+	Active     bool   `json:"active"`
 }
 
 func filterActive(hosts []Host) []Host {
@@ -83,6 +135,8 @@ func filterActive(hosts []Host) []Host {
 	for _, host := range hosts {
 		if host.Active {
 			activeHosts = append(activeHosts, host)
+		} else {
+			h.appendData("disactive.json", host.Address)
 		}
 	}
 	return activeHosts
@@ -111,9 +165,24 @@ func checkErr(at string, err error) {
 	}
 }
 
+// TODO test zip function
+//  zipfile.zip and clientName
+func zip(client *goph.Client, zipfile, dir string) error {
+	// zip the client bot app
+	cmd, err := client.Command("zip", "-r", zipfile+".zip", dir)
+	if err != nil {
+		return err
+	}
+	err = cmd.Run()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 /*
 	// zip the client bot app
-	cmd, err := client.Command("zip", "-r", "lilgo.zip ", "lilgo")
+	cmd, err := client.Command("zip", "-r", "lilgo.zip", "lilgo")
 	checkErr("client.Command():", err)
 
 	err = cmd.Run()
@@ -126,10 +195,55 @@ func checkErr(at string, err error) {
 	log.Println("Download botApp.zip, Done")
 */
 
-// zip -r lilgo.zip lilgo
+/*
+zip -r lilgo.zip lilgo
 // Check is evrything is ok by executing ls command
-//out, err := client.Run("ls")
+out, err := client.Run("ls")
 
-//if err != nil {
-//	log.Fatal(err)
-//}
+if err != nil {
+	log.Fatal(err)
+}
+*/
+
+/*
+exitbot("http://localhost:8080/" + "exit")
+	fmt.Println("done exit bot")
+*/
+
+// exitBot
+func exitBot() {
+	ch := make(chan bool, 1)
+	http.HandleFunc("/exit", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "will Done") //": %s\n", r.URL.Path)
+		ch <- true
+	})
+	go func() {
+		fmt.Println(http.ListenAndServe(":8080", nil))
+	}()
+
+	go func() {
+		if <-ch {
+			fmt.Println("Done")
+			os.Exit(0)
+		}
+	}()
+}
+
+// send exitbot
+func sendExit(address string) {
+	resp, err := http.Get("http://" + address + ":8080/exit")
+	if err != nil {
+		log.Fatal("Error getting response. ", err)
+	}
+	defer resp.Body.Close()
+
+	// Read body from response
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Error reading response. ", err)
+	}
+
+	fmt.Printf("body is : %s\n", body)
+}
+
+const psw = "d7ombot123"
