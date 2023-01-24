6003c0bde329fcd3bd62e86393b092d8510280e3
bashery <ahmedalbachiry@gmail.com>
2023-01-24 13:59:02 +0300

    new nice tool

---
+++ transfare/go.mod
@@ -0,0 +1,11 @@
+module transphare
+
+go 1.19
+
+require github.com/povsister/scp v0.0.0-20210427074412-33febfd9f13e
+
+require (
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/sys v0.0.0-20211031064116-611d5d643895 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+)
--- transfare/main.go
+++ transfare/main.go
@@ -1,22 +1,51 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"io/ioutil"
 
-	"github.com/melbahja/goph"
+	"github.com/povsister/scp"
+)
+
+var (
+	localPath  = "local paht here"
+	remotePath = "rempte path here"
+	remoteIP   = "type remote path here"
+	ipAddres   = "remote ip addres here" // prefere to read it from sepatrate file
+	password   = ""                      //remote pass must be in serparate file . use Getpass function
 )
 
 func main() {
-	fmt.Println("vim-go")
+
+	err := transfare(ipAddres, password)
+	fmt.Println(err)
 }
 
-func transfare() {
+func transfare(remoteIP, password string) error {
+	// conf
+	sshConf := scp.NewSSHConfigFromPassword("root", password)
 
-	sshcli, err := goph.NewUnknown("root", addr /*"139.162.100.216"*/, goph.Password(h.getPass()))
+	// new client
+	scpClient, err := scp.NewClient(remoteIP, sshConf, &scp.ClientOption{})
 	if err != nil {
-		return errors.New("new connect err" + err.Error())
+		return err
 	}
-	defer sshcli.Close()
 
+	return upload(scpClient, localPath, remotePath)
+}
+
+func upload(client *scp.Client, localPath, remotePath string) error {
+	// upload file
+	return client.CopyFileToRemote(localPath, remotePath, &scp.FileTransferOption{})
+
+}
+
+// to scure app read pass form seprite file
+func Getpass() string {
+	data, err := ioutil.ReadFile(".mypass")
+	if err != nil {
+		return err.Error()
+	}
+	psw := string(data)
+	return psw[:len(psw)-1]
 }
