3b3095b9af5e0a17cc874c64e62ab4201feee9f2
bashery <ahmedalbachiry@gmail.com>
2023-07-05 12:12:04 +0300

    improve testing

--- engine_test.go
+++ engine_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"testing"
 )
@@ -20,7 +19,7 @@ func Test_NewIndex(t *testing.T) {
 		NewIndex(i, file)
 	}
 
-	// we need this inline func
+	// this func for get exact endix location in file "At"
 	res := func(i int64) int64 { return (i % 1000) * 20 }
 
 	// input 1
@@ -28,28 +27,29 @@ func Test_NewIndex(t *testing.T) {
 	if pageName != "0" {
 		t.Error("pageName must be 1")
 	}
-	if indx != res(1) {
-		t.Error("index must be 2800")
+	if indx != res(1) { // (1 % 1000) * 20 = 20
+		t.Error("index must be 20")
 	}
 
-	//"input 140
+	//"input 140 return 2800
 	pageName, indx = GetIndex(140)
 	if pageName != "0" {
 		t.Error("pageName must be 1")
 	}
-	if indx != res(140) {
+	if indx != res(140) { // 2800
 		t.Error("index must be 2800")
 	}
 
-	//"input 1111:
+	//"input 1111: 2220
 	pageName, indx = GetIndex(1111)
 	if pageName != "1" {
 		t.Error("pageName must be 1")
 	}
+
 	if indx != res(1111) {
-		t.Error("index must be 2800")
+		t.Error("index must be 2220")
 	}
-	fmt.Println("NewIndex Done")
+	println("NewIndex \t Done")
 
 }
 
@@ -62,6 +62,7 @@ func Test_GetIndex(t *testing.T) {
 	if page != "111" {
 		t.Fatal("page must be 222 not", at)
 	}
+	println("GetIndex \t Done")
 }
 
 func Test_ConvIndex(t *testing.T) {
@@ -73,4 +74,6 @@ func Test_ConvIndex(t *testing.T) {
 	if size != 222 {
 		t.Fatal("size must ber 222 not", size)
 	}
+
+	println("ConvIndex \t Done")
 }
