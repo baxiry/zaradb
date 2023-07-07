f4319dd5c50b19fc31c3073935ebce4396aac49e
bashery <48377058+bashery@users.noreply.github.com>
2023-07-07 19:42:42 +0300

    Update engine_test.go
    Remove useless test func

--- engine_test.go
+++ engine_test.go
@@ -58,29 +58,3 @@ func Test_NewIndex(t *testing.T) {
 	println("NewIndex \t Done")
 
 }
-
-/*
-	func Test_GetIndex(t *testing.T) {
-		id := 111222
-		page, at, _ := GetIndex(id, file)
-		if at != 222*IndexLen {
-			t.Fatal("at must be 111 not", at)
-		}
-		if page != "111" {
-			t.Fatal("page must be 222 not", at)
-		}
-		println("GetIndex \t Done")
-	}
-*/
-func Test_ConvIndex(t *testing.T) {
-	location := "111 222   "
-	at, size := ConvIndex(location)
-	if at != 111 {
-		t.Fatal("at must ber 111 not", at)
-	}
-	if size != 222 {
-		t.Fatal("size must ber 222 not", size)
-	}
-
-	println("ConvIndex \t Done")
-}
