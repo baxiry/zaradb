0a52b1565641876ef57ce36f69a27ac46246f663
bashery <ahmedalbachiry@gmail.com>
2023-07-21 11:30:17 +0300

    create websocket network to connect with this database

--- main.go
+++ main.go
@@ -1,32 +1,69 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
 	"os"
+	"os/signal"
+	"time"
 
-	"github.com/tidwall/gjson"
+	"github.com/labstack/echo"
+	"github.com/lesismal/nbio/nbhttp"
+	"github.com/lesismal/nbio/nbhttp/websocket"
 )
 
 const HelpMessage = "dont forget arguments!"
 const ErrTypeArg = "agruments must be json!"
 
-// this func just for quick test this app
+func onWebsocket(c echo.Context) error {
+	w := c.Response()
+	r := c.Request()
+	upgrader := websocket.NewUpgrader()
+	upgrader.OnMessage(func(c *websocket.Conn, messageType websocket.MessageType, data []byte) {
+		// echo
+		c.WriteMessage(messageType, data)
+	})
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return err
+	}
+	conn.OnClose(func(c *websocket.Conn, err error) {
+		log.Println("OnClose:", c.RemoteAddr().String(), err)
+	})
+	log.Println("OnOpen:", conn.RemoteAddr().String())
+	return nil
+}
+
 func main() {
 
-	args := os.Args
-	if len(args) == 1 {
-		fmt.Println(HelpMessage)
-		return
+	// network ws/http
+	e := echo.New()
+
+	e.GET("/ws", onWebsocket)
+
+	svr := nbhttp.NewServer(nbhttp.Config{
+		Network: "tcp",
+		Addrs:   []string{"localhost:8080"},
+	}, e, nil)
+
+	err := svr.Start()
+	if err != nil {
+		log.Fatalf("nbio.Start failed: %v\n", err)
 	}
 
-	arg := args[1]
+	log.Println("serving [labstack/echo] on [nbio]")
 
-	fmt.Println("arg is ", arg)
+	// database
 
-	action := gjson.Get(arg, "action")
-	data := gjson.Get(arg, "data")
-	fmt.Println("action : ", action)
-	fmt.Println("data   : ", data)
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+	<-interrupt
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+	svr.Shutdown(ctx)
+
 }
 
 /*
