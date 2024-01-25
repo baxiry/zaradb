560b1bdd7a3d64787bd978cd9e8d4d2f54d48808
bashery <ahmedalbachiry@gmail.com>
2024-01-25 15:00:24 +0300

    main func

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-	"kvlite"
-	"time"
 )
 
 var max = 10
 
-func cmain() {
+func main() {
 	var i uint64
 
 	w, err := NewWriter("mylog")
@@ -30,38 +28,4 @@ func cmain() {
 		}
 		println(string(d))
 	}
-}
-
-// 4_294_967_295
-// main
-func main() {
-
-	db := kvlite.Open("db1/")
-	defer db.Close()
-
-	s := time.Now()
-	for i := 0; i < max; i++ {
-		db.Insert("users", " hello world:"+fmt.Sprint(i+1))
-	}
-	fmt.Print("insert result : ")
-	fmt.Println(time.Since(s))
-
-	s = time.Now()
-
-	for i := 0; i < max; i++ {
-		_ = db.Get(i, "users")
-	}
-	fmt.Print("get result : ")
-	fmt.Println(time.Since(s))
-	//time.Sleep(time.Second * 20)
-
-	db.Delete(3, "users")
-	fmt.Println("delete result : ")
-	for i := 122; i < 124; i++ {
-		fmt.Println(123, db.Get(123, "users"))
-	}
-
-	db.Update(3, "users", "new data")
-	fmt.Println("update result : ")
-	fmt.Println(3, db.Get(3, "users"))
 }
