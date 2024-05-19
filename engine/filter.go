b189ca56fa43447b17e16eef16cdebbda17c95aa
baxiry <ahmedalbachiry@gmail.com>
2024-05-19 22:28:01 +0300

    add  &  operators

--- engine/filter.go
+++ engine/filter.go
@@ -9,28 +9,26 @@ import (
 // gjson.Type :
 // json-array:5, int:2, string:3
 
+// {$and:[{name:{$eq:"adam"}},{name:{$eq:"jawad"}}]}
+
+// {$or: [{name:{$eq:"adam"}}, {name:{$eq:"jawad"}}] }
+
 // match verifies that data matches the conditions
 func match(filter, data string) (result bool, err error) {
 	// TODO sould ber return syntax error if op unknown
 
+	fmt.Println("Data is  : ", data)
 	result = true
 
 	gjson.Parse(filter).ForEach(func(qk, qv gjson.Result) bool {
 
 		dv := gjson.Get(data, qk.String())
-		//fmt.Println("q value type : ", qv.Type)
-
-		if qk.String() == "$and" {
-			// fmt.Println("query key is $and")
-		}
-		if qk.String() == "$or" {
-			// fmt.Println("query key is $or")
-		}
 
 		if qv.Type == 5 { // 5:json, int:2, string:3
 			qv.ForEach(func(sqk, sqv gjson.Result) bool {
+
 				if sqv.Type == 3 { // 3:string,
-					fmt.Println("here with: ", sqk.String())
+					//fmt.Println("here with: ", sqk.String())
 
 					switch sqk.String() {
 					case "$gt":
@@ -66,6 +64,13 @@ func match(filter, data string) (result bool, err error) {
 						if dv.String() == sqv.String() {
 							result = false
 						}
+						return result
+
+					case "$or": // not in
+
+						fmt.Println("my be we here, sqk: ", sqk.String())
+
+						result = false
 						return result
 
 					default:
@@ -114,7 +119,6 @@ func match(filter, data string) (result bool, err error) {
 					}
 					return result
 				case "$in": // in array
-					//if sqv.IsArray() {}
 					for _, v := range sqv.Array() {
 						if dv.String() == v.String() {
 							return result
@@ -133,7 +137,34 @@ func match(filter, data string) (result bool, err error) {
 					return result
 
 				default:
-					fmt.Println("why iam here ?", sqk.String())
+
+					if qk.Str == "$and" {
+
+						for _, v := range qv.Array() {
+							fmt.Print("value : ", v)
+							fmt.Print("data : ", data)
+							res, _ := match(v.String(), data)
+							if !res {
+								result = false
+								return result
+							}
+						}
+						return result
+					}
+
+					if qk.Str == "$or" {
+
+						for _, v := range qv.Array() {
+
+							res, _ := match(v.String(), data)
+							if res {
+								return result
+							}
+						}
+						result = false
+						return result
+					}
+
 					err = fmt.Errorf("unknown %s operation", sqk.String())
 					result = false
 					return result
