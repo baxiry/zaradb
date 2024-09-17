87cd630144abf06b06980fd85ce7c24c134607a2
baxiry <ahmedalbachiry@gmail.com>
2024-09-17 22:30:57 +0300

    some test case for match

--- engine/filter_test.go
+++ engine/filter_test.go
@@ -1,51 +1,45 @@
 package engine
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/tidwall/gjson"
 )
 
-// var query = `{"name":"adam","age": 21, "email": "jamal@email.com", "tele":"002345678"}}` //,"contact":{"home":12, "work":45, "tele":{"first":111, "Second":222}}}}`)
-// var query = `{"name":"adam","age": {"$lte":30, "$gte":18}, "email": "jamal@email.com"}, "tele":"001234567"}}` //,"contact":{"home":12, "work":45, "tele":{"first":111, "Second":222}}}}`)
-var query = `{"age": {"$lte":30, "$gte":18}}` //,"contact":{"home":12, "work":45, "tele":{"first":111, "Second":222}}}}`)
+var testCases = []struct {
+	filter   string
+	data     string
+	expected bool
+}{
+	{`{"name":"adam"}`, `{"name":"adam"}`, true},
+	{`{"name":"adam"}`, `{"name":"kamal"}`, false},
+}
 
-var data_ = `{"name":"adam","age": 18, "email": "jamal@email.com", "tele":"001234567"}}`
+func TestMatch(t *testing.T) {
+	// الحالات الاختبارية
 
-func Test_match(t *testing.T) {
-
-	result := make([]string, 0)
-	for _, v := range data {
-		ok, _ := match(gjson.Parse(query), v)
-
-		if ok {
-			result = append(result, v)
+	// تنفيذ الحالات الاختبارية باستخدام t.Run
+	for _, tcase := range testCases {
+		result, _ := match(gjson.Parse(tcase.filter), tcase.data)
+		if result != tcase.expected {
+			t.Errorf("For input %q and pattern %q, expected %v but got %v", tcase.filter, tcase.data, tcase.expected, result)
 		}
 	}
-
-	for k, v := range result {
-		fmt.Println(k, v)
-	}
-	fmt.Println("len result is ", len(result))
-	//res := valid(query, data_)
-	//fmt.Println("\nres : ", res)
-
 }
 
 var data = []string{
-	`{"name":"adam",  "age": 18, "email": "jamal@email.com", "tele":"001234567"}}`,
-	`{"name":"karim", "age": 37, "contact":{"email": "karim@email.com", "tele":002345678 }}`,
-	`{"name":"jawad", "age": 24, "contact":{"email": "jawad@email.com", "tele":001234567 }}`,
-	`{"name":"imane", "age": 26, "contact":{"email": "imane@email.com", "tele":002345030 }}`,
-	`{"name":"adams", "age": 31, "contact":{"email": "adams@email.com", "tele":002345000 }}`,
-	`{"name":"johns", "age": 11, "contact":{"email": "adams@email.com", "tele":002345010 }}`,
-	`{"name":"jawar", "age": 10, "contact":{"email": "jawar@email.com", "tele":002345022 }}`,
-	`{"name":"kadir", "age": 13, "contact":{"email": "kadir@email.com", "tele":002345033 }}`,
-	`{"name":"hamid", "age": 16, "contact":{"email": "hamid@email.com", "tele":002345004 }}`,
-	`{"name":"rajab", "age": 18, "contact":{"email": "rajab@email.com", "tele":002345005 }}`,
-	`{"name":"samir", "age": 19, "contact":{"email": "samir@email.com", "tele":002345006 }}`,
-	`{"name":"akram", "age": 21, "contact":{"email": "akram@email.com", "tele":002345007 }}`,
-	`{"name":"rabih", "age": 31, "contact":{"email": "rabih@email.com", "tele":002345008 }}`,
-	`{"name":"monir", "age": 31, "contact":{"email": "monir@email.com", "tele":002345009 }}`,
+
+	`{"_id": 2, "name":"karim", "age":37, "contact":{"email": "karim@email.com", "tele": "00234856781"}}`,
+	`{"_id": 3, "name":"jawad", "age":24, "contact":{"email": "jawad@email.com", "tele": "00123845672"}}`,
+	`{"_id": 4, "name":"imane", "age":26, "contact":{"email": "imane@email.com", "tele": "00239850303"}}`,
+	`{"_id": 5, "name":"adams", "age":31, "contact":{"email": "adams@email.com", "tele": "00234850004"}}`,
+	`{"_id": 6, "name":"johns", "age":11, "contact":{"email": "adams@email.com", "tele": "00234850105"}}`,
+	`{"_id": 7, "name":"jawar", "age":10, "contact":{"email": "jawar@email.com", "tele": "00234850226"}}`,
+	`{"_id": 8, "name":"kadir", "age":13, "contact":{"email": "kadir@email.com", "tele": "00238450337"}}`,
+	`{"_id": 9, "name":"hamid", "age":16, "contact":{"email": "hamid@email.com", "tele": "00238450048"}}`,
+	`{"_id":10, "name":"rajab", "age":18, "contact":{"email": "rajab@email.com", "tele": "00230450059"}}`,
+	`{"_id":11, "name":"samir", "age":19, "contact":{"email": "samir@email.com", "tele": "00230450060"}}`,
+	`{"_id":12, "name":"akram", "age":21, "contact":{"email": "akram@email.com", "tele": "00239450071"}}`,
+	`{"_id":13, "name":"rabih", "age":31, "contact":{"email": "rabih@email.com", "tele": "00230450083"}}`,
+	`{"_id":14, "name":"monir", "age":31, "contact":{"email": "monir@email.com", "tele": "00239450094"}}`,
 }
