9f07fae3d39b66a7038a432c14666d92fab62918
baxiry <ahmedalbachiry@gmail.com>
2024-09-19 03:19:42 +0300

    add IDs for every test cases, & some test cases

--- engine/filter_test.go
+++ engine/filter_test.go
@@ -7,22 +7,56 @@ import (
 )
 
 var testCases = []struct {
-	filter   string
-	data     string
-	expected bool
+	filter  string
+	data    string
+	isMatch bool
+	caseid  int
 }{
-	{`{"name":"adam"}`, `{"name":"adam"}`, true},
-	{`{"name":"adam"}`, `{"name":"kamal"}`, false},
+	// ....filter.....     ...data...    ...isMatch...
+	//  cases of numbers
+	{`{"age": 18}`, `{"age": 18}`, true, 0},
+	{`{"age": 18}`, `{"age": 19}`, false, 1},
+
+	{`{"age":{"$eq":18}}`, `{"age": 18}`, true, 2},
+	{`{"age":{"$eq":18}}`, `{"age": 19}`, false, 3},
+
+	{`{"age":{"$ne": 18}}`, `{"age": 19}`, true, 4},
+	{`{"age":{"$ne": 18}}`, `{"age": 18}`, false, 5},
+
+	{`{"age":{"$gt": 18}}`, `{"age": 19}`, true, 6},
+	{`{"age":{"$gt": 18}}`, `{"age": 18}`, false, 7},
+	{`{"age":{"$gt": 18}}`, `{"age": 17}`, false, 8},
+
+	{`{"age":{"$lt": 18}}`, `{"age": 17}`, true, 9},
+	{`{"age":{"$lt": 18}}`, `{"age": 18}`, false, 10},
+	{`{"age":{"$lt": 18}}`, `{"age": 19}`, false, 11},
+
+	{`{"age":{"$gte": 18}}`, `{"age": 19}`, true, 12},
+	{`{"age":{"$gte": 18}}`, `{"age": 18}`, true, 13},
+	{`{"age":{"$gte": 18}}`, `{"age": 17}`, false, 14},
+
+	{`{"age":{"$lte": 18}}`, `{"age": 17}`, true, 15},
+	{`{"age":{"$lte": 18}}`, `{"age": 18}`, true, 16},
+	{`{"age":{"$lte": 18}}`, `{"age": 19}`, false, 17},
+
+	// string cases
+	{`{"name":"adam"}`, `{"name":"adam"}`, true, 18},
+	{`{"name":"adam"}`, `{"name":"kamal"}`, false, 19},
+
+	{`{"name":{"$eq":"adam"}}`, `{"name":"adam"}`, true, 20},
+	{`{"name":{"$eq":"adam"}}`, `{"name":"john"}`, false, 21},
+
+	{`{"name":{"$ne":"adam"}}`, `{"name":"john"}`, true, 22},
+	{`{"name":{"$ne":"adam"}}`, `{"name":"adam"}`, false, 23},
 }
 
-func TestMatch(t *testing.T) {
-	// الحالات الاختبارية
+func Test_Match(t *testing.T) {
 
-	// تنفيذ الحالات الاختبارية باستخدام t.Run
 	for _, tcase := range testCases {
 		result, _ := match(gjson.Parse(tcase.filter), tcase.data)
-		if result != tcase.expected {
-			t.Errorf("For input %q and pattern %q, expected %v but got %v", tcase.filter, tcase.data, tcase.expected, result)
+		if result != tcase.isMatch {
+			t.Errorf("\n-----caseId: %d-------\nfilter: %s\n data: %s\nexpected %v",
+				tcase.caseid, tcase.data, tcase.filter, tcase.isMatch)
 		}
 	}
 }
